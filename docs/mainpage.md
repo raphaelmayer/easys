3257e5a6959b2b42fd704a150379acf2fb61634b
Raphael Mayer <35350266+raphaelmayer@users.noreply.github.com>
2025-06-27 00:02:49 +0200

    Automatically generate and publish documentation (#13)
    * Add Doxygen comments and CI workflow
    
    * Adjusting docs mainpage
    
    * Update README and examples

---
+++ docs/mainpage.md
@@ -0,0 +1,101 @@
+\mainpage Easys::ECS Documentation
+
+\brief
+
+EasyS is a minimalist, header-only C++ library designed to streamline the development of applications using the Entity Component System (ECS) architecture. With a focus on simplicity, flexibility, and ease of use, it offers developers an unopinionated foundation to build efficient, high-performance systems without the overhead of external dependencies.  
+
+**The library uses a static design approach**, requiring all component types to be registered at compile time through template parameters.  
+
+\tableofcontents
+
+## Easys::ECS
+
+The ```Easys::ECS``` class is the single public interface for EasyS. It provides all functionality for:
+
+- Compile-time component registration  
+- Entity creation and management  
+- Component addition, removal, and querying  
+
+All public operations are available as member functions. See the API Reference for a complete list and descriptions.
+
+## Quick Start Guide
+
+This basic example demonstrates how to create entities, add components, and retrieve them:
+```
+#include <easys/easys.hpp>
+#include <iostream>
+
+// This is a very basic example to demonstrate creating an entity,
+// adding a component to it, and retrieving the component.
+
+// Define a Position component structure.
+// Components are simple structs or classes that contain data.
+struct Position {
+	float x, y;
+};
+
+int main()
+{
+	using namespace Easys;
+  
+	// Create an instance of the ECS class and register all component types.
+	ECS<Position> ecs;
+
+	// Create a new entity. An entity is just a number.
+	Entity entity = ecs.addEntity();
+
+	// Add a Position component to the entity with initial values.
+	ecs.addComponent<Position>(entity, {10.0f, 20.0f});
+
+	// Update the component.
+	auto &component = ecs.getComponent<Position>(entity);
+	component.x = 0.0f;
+
+	// Retrieve the Position component from the entity to read or modify it.
+	Position &pos = ecs.getComponent<Position>(entity);
+	std::cout << "Position: " << pos.x << ", " << pos.y << std::endl;
+
+	return 0;
+}
+```
+
+\note For more usage examples, take a look at the [examples](https://github.com/raphaelmayer/easys/blob/master/examples).
+
+## Configuration Options
+
+EasyS provides a couple of overrideable macros to tailor the ECS to your use case:
+
+- \ref EASYS_ENTITY_TYPE - define the integer type used for entity IDs (default: `uint32_t`)
+- \ref EASYS_ENTITY_LIMIT - set the maximum number of active entities (default: `10000`) 
+
+Define these macros **before** including EasyS, either in source or via compiler flags.
+
+### Customizing in Source Code
+
+```
+#define EASYS_ENTITY_TYPE uint64_t // Change to 64-bit unsigned integer
+#define EASYS_ENTITY_LIMIT 50000   // Increase entity limit
+#include <easys/easys.hpp>         // Path to the configuration file
+```
+
+### Customizing with Compiler Flags
+
+**GCC/Clang** on Linux/macOS:
+```
+g++ -DEASYS_ENTITY_TYPE=uint64_t -DEASYS_ENTITY_LIMIT=50000 -o my_app my_app.cpp
+```
+
+**MSVC** on Windows:
+```
+cl /D EASYS_ENTITY_TYPE=uint64_t /D EASYS_ENTITY_LIMIT=50000 my_app.cpp
+```
+
+Depending on your compiler and development environment the exact command might be slightly different.
+
+## Philosophy and User Responsibilities
+
+\note Users are expected to:
+- **Register all Components at Compile-Time:** All component types must be specified as template parameters when instantiating the ECS. Accessing a foreign component will result in a compiler error.
+- **Check Component Presence Before Access:** Always verify that an entity has a given component before accessing it. Calling `Easys::ECS::getComponent<T>(entity)` on an entity that lacks `T` will throw a `Easys::KeyNotFoundException`.
+
+
--- include/easys/ecs.hpp
+++ include/easys/ecs.hpp
@@ -11,9 +11,17 @@
 
 namespace Easys {
 
+/**
+ * @brief Manages entities and components in an Entity-Component-System architecture.
+ * @tparam AllComponentTypes A list of all possible component types that can be used in this ECS instance.
+ */
 template <typename... AllComponentTypes>
 class ECS {
    public:
+	/**
+	 * @brief Initializes the ECS with a predefined maximum number of entities (MAX_ENTITIES).
+	 * @details All entity IDs are initially available for assignment.
+	 */
 	ECS()
 	{
 		for (Entity entity = 0; entity < MAX_ENTITIES; entity++)
@@ -22,12 +30,17 @@ class ECS {
 		}
 	}
 
-	// This constructor is for initialising the ECS with a set of specific
-	// entities. I decided against an addEntity(Entity) method to discourage
-	// tampering with entities too much. I think this really should be the ECS's
-	// responsibility.
+	/**
+	 * @brief Initializes the ECS with a specific set of entities.
+	 * @details This constructor is useful for creating a new ECS instance based on a subset
+	 * of entities from another instance or a predefined list.
+	 * @param entities A set of entities to initialize the ECS with.
+	 */
 	ECS(const std::set<Entity>& oldEntities)
 	{
+		// I decided against an addEntity(Entity) method to discourage
+		//  tampering with entities too much. I think this really should be the ECS's
+		//  responsibility.
 		for (Entity entity = 0; entity < MAX_ENTITIES; entity++)
 		{
 			if (oldEntities.contains(entity))
@@ -37,6 +50,11 @@ class ECS {
 		}
 	}
 
+	/**
+	 * @brief Adds a new entity to the ECS.
+	 * @return The ID of the newly created entity.
+	 * @throws std::runtime_error if the maximum number of entities (MAX_ENTITIES) is reached.
+	 */
 	inline Entity addEntity()
 	{
 		if (getEntityCount() < MAX_ENTITIES)
@@ -51,6 +69,11 @@ class ECS {
 		throw std::runtime_error("MAX NUMBER OF ENTITIES REACHED!");
 	}
 
+	/**
+	 * @brief Removes an entity and all its associated components from the ECS.
+	 * @details The removed entity's ID is made available for reuse.
+	 * @param e The entity to remove.
+	 */
 	inline void removeEntity(const Entity e)
 	{
 		// Remove all components associated with the entity
@@ -61,75 +84,143 @@ class ECS {
 		availableEntityIds_.push(e);
 	}
 
+	/**
+	 * @brief Checks if an entity exists within the ECS.
+	 * @param e The entity to check for.
+	 * @return True if the entity exists, false otherwise.
+	 */
 	inline bool hasEntity(const Entity e) const { return entities_.contains(e); }
 
+	/**
+	 * @brief Returns a reference to the set of all entities.
+	 * @return A constant reference to the set of all entities currently in the ECS.
+	 */
 	inline const std::set<Entity>& getEntities() const { return entities_; }
 
-	inline size_t getEntityCount() const { return entities_.size(); }
-
-	template <typename T>
-	inline void addComponent(const Entity e, T component)
-	{
-		registry_.addComponent(e, std::move(component));
-	}
-
-	template <typename T>
-	inline void removeComponent(const Entity e)
-	{
-		registry_.template removeComponent<T>(e);
-	}
-
-	template <typename T>
-	inline T& getComponent(const Entity e)
-	{
-		return registry_.template getComponent<T>(e);
-	}
-
-	template <typename T>
-	inline const T& getComponent(const Entity e) const
-	{
-		return registry_.template getComponent<T>(e);
-	}
-
-	template <typename T>
-	inline bool hasComponent(const Entity e) const
-	{
-		return registry_.template hasComponent<T>(e);
-	}
-
+	/**
+	 * @brief Returns a vector of entities that have a component of a specific type.
+	 * @tparam T The component type to query for.
+	 * @return A constant reference to a vector of entities possessing the component.
+	 */
 	template <typename T>
 	inline const std::vector<Entity>& getEntitiesByComponent() const
 	{
 		return registry_.template getEntitiesByComponent<T>();
 	}
 
+	/**
+	 * @brief Returns a vector of entities that have all of the specified component types.
+	 * @tparam Ts A variadic list of component types to query for.
+	 * @return A vector of entities that possess all specified components.
+	 */
 	template <typename... Ts>
 	inline std::vector<Entity> getEntitiesByComponents() const
 	{
 		return registry_.template getEntitiesByComponents<Ts...>();
 	}
 
-	inline size_t getComponentCount() const { return registry_.size(); }
+	/**
+	 * @brief Returns the total number of active entities in the ECS.
+	 * @return The number of entities.
+	 */
+	inline size_t getEntityCount() const { return entities_.size(); }
 
+	/**
+	 * @brief Adds a component of type T to an entity.
+	 * @details If the entity already has a component of type T, it will be updated with the new value.
+	 * @tparam T The type of the component to add.
+	 * @param e The entity to which the component will be added.
+	 * @param c The component data to add.
+	 */
+	template <typename T>
+	inline void addComponent(const Entity e, T component)
+	{
+		registry_.addComponent(e, std::move(component));
+	}
+
+	/**
+	 * @brief Removes a component of type T from an entity.
+	 * @tparam T The type of the component to remove.
+	 * @param e The entity from which to remove the component.
+	 */
+	template <typename T>
+	inline void removeComponent(const Entity e)
+	{
+		registry_.template removeComponent<T>(e);
+	}
+
+	/**
+	 * @brief Retrieves a reference to a component of type T from an entity.
+	 * @tparam T The type of the component to retrieve.
+	 * @param e The entity whose component is to be retrieved.
+	 * @return A mutable reference to the component.
+	 */
+	template <typename T>
+	inline T& getComponent(const Entity e)
+	{
+		return registry_.template getComponent<T>(e);
+	}
+
+	/**
+	 * @brief Retrieves a reference to a component of type T from an entity.
+	 * @tparam T The type of the component to retrieve.
+	 * @param e The entity whose component is to be retrieved.
+	 * @return A immutable reference to the component.
+	 */
+	template <typename T>
+	inline const T& getComponent(const Entity e) const
+	{
+		return registry_.template getComponent<T>(e);
+	}
+
+	/**
+	 * @brief Checks if an entity has a component of type T.
+	 * @tparam T The type of the component to check for.
+	 * @param e The entity to check.
+	 * @return True if the entity has the component, false otherwise.
+	 */
+	template <typename T>
+	inline bool hasComponent(const Entity e) const
+	{
+		return registry_.template hasComponent<T>(e);
+	}
+
+	/**
+	 * @brief Returns the total count of components of the specified types within the ECS.
+	 * @tparam Ts A variadic list of component types. If template parameters are omitted, it returns the total count of
+	 * all component types.
+	 * @return The total number of components of the specified types.
+	 */
 	template <typename... Ts>
 	inline size_t getComponentCount() const
 	{
 		return registry_.template size<Ts...>();
 	}
 
+	inline size_t getComponentCount() const { return registry_.size(); }
+
+	/**
+	 * @brief Clears all entities and components from the ECS.
+	 * @details Resets the ECS to its initial state, making all entity IDs available again.
+	 */
 	inline void clear()
 	{
 		registry_.clear();
 		clearEntities();
 	}
 
-	inline void clearComponents() { registry_.clear(); }
-
+	/**
+	 * @brief Removes components of specific types from all entities within the ECS.
+	 * @tparam Ts A variadic list of component types to clear. If template parameters are omitted, all types of
+	 * components are cleared from all entities.
+	 */
 	template <typename... Ts>
 	inline void clearComponents()
 	{
 		registry_.template clear<Ts...>();
 	}
+
+	inline void clearComponents() { registry_.clear(); }
 
    private:
 	std::queue<Entity> availableEntityIds_;
