355864f520192634a7c3ce55c6a73e919331760c
Raphael Mayer <raphael.mayer@besi.com>
2025-10-14 15:07:04 +0200

    Added basic unit tests

--- include/easys/eventbus.hpp
+++ include/easys/eventbus.hpp
@@ -51,6 +51,9 @@ struct ComponentRemoved {
 	Component component;
 };
 
+// TODO: We could make CallbackId more complex, where the subscription only exists as long as the object is still alive, 
+// i.e. we could cleanup in the dtor, so the user would not have to unsubscribe. 
+// But is this an advantage? Then you would also need to keep the "connection" alive. 
 using CallbackId = uint64_t;
 
 template <typename... Events>
@@ -77,7 +80,20 @@ class Eventbus {
 	template <typename Event, typename Func>
 	CallbackId subscribe(Entity e, Func&& fn)
 	{
-		// TODO: add subscriber to list of subscribers for a specific entity
+		// TODO: add subscriber to list of subscribers for a specific entity. 
+		// does this even make sense? should we let the user handle the "filtering"?
+	
+		// 1: Would be a cheap solution and only pretends to do the thing it should. 
+		// The sub would still get ALL events of type Event, but would reject them, if they are not from the entity in question.
+		auto wrapFn = [&](const Event& ev)
+		{ 
+			if (ev.entity == e)
+				fn(); 
+		};
+		return subscribe<Event>(wrapFn);
+		
+		// 2. Another solution would be to delegate this "filtering" to dispatch, where we call callbacks only if the event is from a subscribed to entity. 
+		// But then we also need to track, which entities (if any single one) are being tracked by which sub. Possibly in Subscriber?
 	}
 
 	// Remove a subscription from the list of subscribers.
