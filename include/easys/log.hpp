4a8718cfefa091f8977d8fb1fb7ecc06ab254e27
Raphael Mayer <raphael_mayer@hotmail.de>
2025-12-22 20:05:28 +0100

    Fix logging verbosity

--- include/easys/log.hpp
+++ include/easys/log.hpp
@@ -85,100 +85,56 @@ constexpr std::string_view level_to_string(LogLevel level)
 	}
 }
 
-struct concise_source_location {
-	std::source_location srcloc;
-
-	constexpr explicit concise_source_location(std::source_location l = std::source_location::current()) noexcept
-	    : srcloc(l)
-	{
-	}
-
-	constexpr std::string_view file_name() const noexcept
-	{
-		const auto& fullPath = srcloc.file_name();
-		// TODO: prettify
-		return fullPath;
-	}
-	constexpr std::string_view function_name() const noexcept
-	{
-		const auto& functionName = srcloc.function_name();
-		// TODO: prettify
-		return functionName;
-	}
-	constexpr std::uint32_t line() const noexcept { return srcloc.line(); }
-	constexpr std::uint32_t column() const noexcept { return srcloc.column(); }
-};
-
-struct verbose_source_location {
-	std::source_location srcloc;
-
-	constexpr explicit verbose_source_location(std::source_location l = std::source_location::current()) noexcept
-	    : srcloc(l)
-	{
-	}
-
-	constexpr std::string_view file_name() const noexcept { return srcloc.file_name(); }
-	constexpr std::string_view function_name() const noexcept { return srcloc.function_name(); }
-	constexpr std::uint32_t line() const noexcept { return srcloc.line(); }
-	constexpr std::uint32_t column() const noexcept { return srcloc.column(); }
-};
-
-struct flex_source_location {
+struct log_location {
 	std::string_view file_name_;
+	std::string_view file_path_;
 	std::string_view function_name_;
+	std::string_view function_signature_;
 	std::uint32_t line_;
 	// std::uint32_t column_;
 
-	constexpr explicit flex_source_location(std::source_location l = std::source_location::current()) noexcept
-	{
-		file_name_ = l.file_name();
-		function_name_ = l.function_name();
-		line_ = l.line();
-	}
-
-	constexpr flex_source_location(std::string_view file, std::uint32_t line, std::string_view funcName) noexcept
+	constexpr log_location(std::string_view file, std::uint32_t line, std::string_view fullFuncName,
+	                               std::string_view funcName) noexcept
 	{
 		// const auto& start = file.find_last_of("/") + 1;
 		// const auto& start = file.find_last_of("\\") + 1;
-		const auto& start = std::min(file.find_last_of("/"), file.find_last_of("\\")) + 1;
+		// const auto& start = std::min(file.find_last_of("/"), file.find_last_of("\\")) + 1;
+		const auto& start = file.find_last_of("/") + 1;  // TODO: seems to suffice on win and linux?
 		const auto& substr = file.substr(start, file.size() - start);
 
+		file_path_ = file;
 		file_name_ = substr;
-
+		function_signature_ = fullFuncName;
 		function_name_ = funcName;
 		line_ = line;
 	}
 
-	constexpr explicit flex_source_location(std::string_view funcName, std::source_location l = std::source_location::current()) noexcept
+	constexpr explicit log_location(std::string_view funcName,
+	                                        std::source_location l = std::source_location::current()) noexcept
 	{
-		file_name_ = l.file_name();
-		function_name_ = funcName;
-		line_ = l.line();
+		log_location(l.file_name(), l.line(), l.function_name(), funcName);
 	}
 
+#if EASYS_LOG_VERBOSITY
+	constexpr std::string_view file_name() const noexcept { return file_path_; }
+	constexpr std::string_view function_name() const noexcept { return function_signature_; }
+#else
 	constexpr std::string_view file_name() const noexcept { return file_name_; }
 	constexpr std::string_view function_name() const noexcept { return function_name_; }
+#endif
+
 	constexpr std::uint32_t line() const noexcept { return line_; }
 	// constexpr std::uint32_t column() const noexcept { return column; }
 };
 
-// #if EASYS_LOG_VERBOSITY
-// using log_location = verbose_source_location;
-// #else
-// using log_location = concise_source_location;
-// #endif
-using log_location = flex_source_location;
-
 // Format log message
-inline void log_impl(LogLevel level, std::string_view message,
-                     log_location location = log_location{std::source_location::current()})
+inline void log_impl(LogLevel level, std::string_view message, log_location location)
 {
 	std::ostringstream oss;
 	oss << "[" << get_timestamp() << "] "
 	    << "[" << level_to_string(level) << "] "
 	    << "[" << location.file_name() << ":" << location.line() << "] "
-	    << "`" << location.function_name() << "`: "
-	    << message;
+	    << "`" << location.function_name() << "`: " << message;
 
 	auto final_message = oss.str();
 	// auto final_message = oss.view(); better?
@@ -194,18 +150,15 @@ inline void log_impl(LogLevel level, std::string_view message,
 	FileWriter::write(final_message);
 }
 
+#define HERE Easys::log::log_location(__FILE__, __LINE__, __FUNCTION__, __func__)
+
 // Base logging macro with compile-time filtering
 #define EASYS_LOG_IMPL(level, msg) \
 	do \
 	{ \
 		if constexpr (Easys::log::is_enabled<level>::value) \
 		{ \
-			Easys::log::log_location logloc; \
-			if constexpr (EASYS_LOG_VERBOSITY) \
-				logloc = Easys::log::log_location(); \
-			else \
-				logloc = Easys::log::log_location(__FILE__, __LINE__, __func__); \
-			::Easys::log::log_impl(level, msg, logloc); \
+			::Easys::log::log_impl(level, msg, HERE); \
 		} \
 	} while (0)
 
@@ -220,29 +173,22 @@ inline void log_impl(LogLevel level, std::string_view message,
 
 class EntryExitLogger {
    public:
-	EntryExitLogger(const std::string_view file, int line, const std::string_view funcName,
-	                log_location location = log_location{std::source_location::current()})
-	    : file_(file), line_(line), funcName_(funcName), location_(location)
+	EntryExitLogger(log_location location) : location_(location)
 	{
-		// EASYS_LOG_TRACE("Entry", location_);
 		if constexpr (Easys::log::is_enabled<LogLevel::EASYS_TRACE>::value)
 			log_impl(LogLevel::EASYS_TRACE, "Entry", location_);
 	}
 
 	~EntryExitLogger()
 	{
-		// EASYS_LOG_TRACE("Exit", location_);
 		if constexpr (Easys::log::is_enabled<LogLevel::EASYS_TRACE>::value)
 			log_impl(LogLevel::EASYS_TRACE, "Exit", location_);
 	}
 
    private:
-	std::string_view file_;
-	int line_;
-	std::string_view funcName_;
 	log_location location_;
 };
 
-#define EASYS_LOG_ENTRY_EXIT Easys::log::EntryExitLogger eel(__FILE__, __LINE__, __func__)
+#define EASYS_LOG_ENTRY_EXIT Easys::log::EntryExitLogger eel(HERE)
 
 }  // namespace Easys::log
